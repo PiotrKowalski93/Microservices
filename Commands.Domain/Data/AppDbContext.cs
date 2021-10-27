411f22c42b6c1a8cce1a1e2640436afbf22f1ce0
Piotr Kowalski <Kowalskii1@op.pl>
2021-10-27 23:09:13 +0200

    Add Commands api domain models and dbcontext

---
+++ Commands.Domain/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Commands.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commands.Domain.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Command> Commands { get; set; }
+    }
+}
---
+++ Commands.Domain/Data/DBContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Commands.Domain.Data
+{
+    public class DBContext
+    {
+    }
+}
